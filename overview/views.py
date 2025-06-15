90618e63cff23595abb1cac5c4a868508c5764fd
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-15 14:41:15 +0700

    Add render function for artiles list HTML page

--- overview/views.py
+++ overview/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
-from .models import PlayerPlayer
-from .serializers import PlayerSerializer
+from .models import PlayerPlayer, ArticleArticle
+from .serializers import PlayerSerializer, ArticleSerializer
 
+# Player info
 class PlayerDetailView(generics.RetrieveAPIView):
     queryset = PlayerPlayer.objects.all()
     serializer_class = PlayerSerializer
@@ -11,3 +12,12 @@ class PlayerDetailView(generics.RetrieveAPIView):
 def player_detail_html(request, id):
     player = get_object_or_404(PlayerPlayer, id=id)
     return render(request, 'overview/player_detail.html', {'player': player})
+
+# Articles info
+class ArticleListView(generics.ListAPIView):
+    queryset = ArticleArticle.objects.all()
+    serializer_class = ArticleSerializer
+
+def articles_list_view(request):
+    articles = ArticleArticle.objects.all()[:10]  # first 10 articles
+    return render(request, 'overview/articles_list.html', {'articles': articles})
