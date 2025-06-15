e2df6bde2f280e3598b6abddd6095fba30bb2593
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-15 12:10:53 +0700

    Create view for player info

--- overview/views.py
+++ overview/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import PlayerPlayer
+from .serializers import PlayerSerializer
 
-# Create your views here.
+class PlayerDetailView(generics.RetrieveAPIView):
+    queryset = PlayerPlayer.objects.all()
+    serializer_class = PlayerSerializer
+
