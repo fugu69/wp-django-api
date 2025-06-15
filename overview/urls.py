c4a77408d995c8f93a6db6aaf3fcda861a988849
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-15 14:42:00 +0700

    Add URL path for articles list HTML page

--- overview/urls.py
+++ overview/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import PlayerDetailView, player_detail_html
+from .views import PlayerDetailView, player_detail_html, ArticleListView, articles_list_view
 
 urlpatterns = [
-    # API response
+    # Player info API response
     path('players/<int:id>/', PlayerDetailView.as_view(), name='player-detail'),
 
     # HTML rendering
     path('players/<int:id>/view/', player_detail_html, name='player-detail-html'),
+
+    # Article info API response
+    path('articles/', ArticleListView.as_view(), name='articles-list'),
+
+    # Article info HTML response
+    path('articles/view/', articles_list_view, name='articles-list-view'),
 ]
