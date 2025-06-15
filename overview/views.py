106fd5846ac9ad3d039e7b938f825c65e4abf483
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-15 13:39:36 +0700

    Add function to render HTML with player info

--- overview/views.py
+++ overview/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
 from .models import PlayerPlayer
 from .serializers import PlayerSerializer
@@ -5,4 +6,8 @@ from .serializers import PlayerSerializer
 class PlayerDetailView(generics.RetrieveAPIView):
     queryset = PlayerPlayer.objects.all()
     serializer_class = PlayerSerializer
+    lookup_field = 'id'
 
+def player_detail_html(request, id):
+    player = get_object_or_404(PlayerPlayer, id=id)
+    return render(request, 'overview/player_detail.html', {'player': player})
