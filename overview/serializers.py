38a0697ec0efd401a60de1c720b6a6b528e6aa8c
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-15 14:05:06 +0700

    Add article serializer

--- overview/serializers.py
+++ overview/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import PlayerPlayer
+from .models import PlayerPlayer, ArticleArticle
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlayerPlayer
         fields = ['id', 'image', 'energy', 'gold', 'time_zone']
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleArticle
+        fields = ['title']
