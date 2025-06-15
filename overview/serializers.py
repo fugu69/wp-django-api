ce65e0a9a9df065ea860eada43745c6ecfac970a
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-15 12:05:50 +0700

    Create serializer for PlayerPlayer model

---
+++ overview/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import PlayerPlayer
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlayerPlayer
+        fields = '__all__'  # You can limit fields later if needed
