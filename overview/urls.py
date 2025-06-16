e190a3c5d59efd3a9cbd0c967a3a89c281d0f9c0
Maximus Brutalis <primeskobelev@gmail.com>
2025-06-16 09:36:28 +0700

    Add Article CRUD urls

--- overview/urls.py
+++ overview/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import PlayerDetailView, player_detail_html, ArticleListView, articles_list_view
+from .views import (
+    PlayerDetailView, 
+    player_detail_html, 
+    ArticleListView, 
+    articles_list_view,
+    articles_list_view,
+    article_create,
+    article_update,
+    article_delete,
+)
 
 urlpatterns = [
     # Player info API response
@@ -13,4 +22,10 @@ urlpatterns = [
 
     # Article info HTML response
     path('articles/view/', articles_list_view, name='articles-list-view'),
+
+    # Article CRUD
+    path('articles/view/', articles_list_view, name='articles-list-view'),
+    path('articles/create/', article_create, name='article-create'),
+    path('articles/<int:pk>/update/', article_update, name='article-update'),
+    path('articles/<int:pk>/delete/', article_delete, name='article-delete'),
 ]
